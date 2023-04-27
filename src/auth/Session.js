3f14aa3327f91985f356b9b52fd2046dfe39a599
conghaile <quandaviousgooch@proton.me>
2023-04-26 22:59:30 -0600

    Added middleware that checks request cookie and adds loggedIn boolean and email fields to req object if present, added ensure-login and logout endpoints that handle session token w/ redis.

--- src/auth/Session.js
+++ src/auth/Session.js
@@ -1,7 +1,11 @@
 import { randomBytes } from 'crypto'
 
-export const newSession = async () => {
-    return await randomBytes(32).toString('hex')
+export const newSession = async (email, client) => {
+    const token = randomBytes(32).toString('hex')
+    await client.set(token, email, {
+        EX: 604800,
+    })
+    return token
 }
 
 export default { newSession }
--- src/server.js
+++ src/server.js
@@ -7,18 +7,45 @@ import { timeframeMap, bucketizeMap } from './coin_logic/constants.js'
 import bucketize from './coin_logic/bucketize.js'
 import bodyParser from 'body-parser'
 import { newSession } from './auth/Session.js'
+import { createClient } from 'redis'
+import cookieParser from 'cookie-parser'
 const { Client } = pkg;
+
+const redisClient = createClient()
+redisClient.on('error', err => console.log('Redis Client Error:', err))
+await redisClient.connect()
 
 dotenv.config()
 
 const app = express()
+
+const checkSession = async (req, res, next) => {
+    const cookie = req.cookies.SESSION_ID
+    if (cookie !== undefined) {
+        const email = await redisClient.get(cookie)
+        if (email !== null) {
+            req.email = email
+            req.loggedIn = true
+        } else {
+            req.loggedIn = false
+            
+        }
+    } else {
+        req.loggedIn = false
+    }
+    next()
+}
+
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    credentials: true
 }))
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: false,
 }))
 app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(checkSession)
 
 const PORT = Number(process.env.PORT)
 const TABLE = process.env.TABLE
@@ -59,7 +86,14 @@ app.post('/native-login', async (req, res) => {
     const handler = new PgHandler(client, TABLE)
     const result = await handler.login(req.body.email, req.body.password)
     if (result) {
-        console.log("Logged in!")
+        // Generate and store session token
+        const sessionToken = await newSession(req.body.email, redisClient)
+        // Send session token in response
+        res.cookie('SESSION_ID', sessionToken, {
+            maxAge: 604800000,
+            sameSite: 'none',
+            secure: true
+        })
         res.sendStatus(200)
     } else {
         res.sendStatus(401)
@@ -70,7 +104,30 @@ app.post('/native-signup', async (req, res) => {
     const handler = new PgHandler(client, TABLE)
     const result = await handler.newUser(req.body.email, req.body.password)
     if (result == 1) {
+        //Generate and store session token
+        const sessionToken = await newSession(req.body.email, redisClient)
+        res.cookie('SESSION_ID', sessionToken, {
+            maxAge: 604800000,
+            
+        })
         res.sendStatus(200)
+    } else {
+        res.sendStatus(401)
+    }
+})
+
+app.get('/logout', async (req, res) => {
+    if (req.loggedIn) {
+        await redisClient.hDel(req.cookies.SESSION_ID)
+    }
+    res.sendStatus(200)
+})
+
+app.get('/ensure-login', async (req, res) => {
+    if (req.loggedIn) {
+        res.json({
+            email: req.email
+        })
     } else {
         res.sendStatus(401)
     }
